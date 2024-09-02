380f7418ea961c15dad1ddbf87cc94b1d31b3a69
Danish khan <danishyazdani063@gmail.com>
2024-09-02 21:42:43 +0500

    first commit

---
+++ account/home/urls.py
@@ -0,0 +1,9 @@
+
+# from django.contrib import admin
+# from django.urls import path,include
+# from home.views import *
+# urlpatterns = [
+#     path("home/", home.views),
+#     # path('admin/', admin.site.urls),
+# ]
+
