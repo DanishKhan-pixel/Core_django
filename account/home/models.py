676449272c02819bd7a153e9e7ada75451747148
Danish khan <danishyazdani063@gmail.com>
2024-09-04 11:51:44 +0500

    update

--- account/home/models.py
+++ account/home/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Teacher(models.Model):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    subject = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    date_of_hire = models.DateField()
+    
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} - {self.subject}"
+    
+
+class Car(models.Model):
+    name=models.CharField(max_length=23)
+    speed=models.IntegerField(default=30)
