9eb4fefca3cb96647b82abf58846dc4ad7052b67
Danish khan <danishyazdani063@gmail.com>
2024-09-04 11:53:43 +0500

    update

--- account/home/models.py
+++ account/home/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
 # Create your models here.
-
 class Teacher(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
